12ec186cef2b2e3acc8eb25838cbd0a800fbc417
joby foster <jobyjfoster@gmail.com>
2023-08-24 13:25:17 -0500

    Big changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
   // }
   return (
     <div className='site'>
+
       <header className='site-header'>
+        
         <nav id='site-nav'>
           
           <p><a href='/'>home</a></p>
@@ -20,19 +22,27 @@ function App() {
           
           
         </nav>
+
       </header>
+
       <div className='site-content'>
+
         <div className='site-aboutme'>
+
           <p id='name'>JOBY FOSTER</p>
 
           <div id='site-aboutme-text'>
+
             <p>
               Graduated from <a rel='noreferrer' id='shs-name' href='https://www.starkvillesd.com/starkville-high-school/'>Starkville High School</a> in 2023 as a motivated coder with a strong background in HTML, CSS, JavaScript, Python, SQL, a CTE distinguished diploma, and a CSpire Certification.
               Currently amplifying front and back-end development skills at <a id='bcca-name' rel="noreferrer" href='https://basecampcodingacademy.org/' target='_blank'>Base Camp Coding Academy</a>, graduating  by May 2024.
               Aiming to apply my passion for coding to solve real-world problems and promote innovation.
             </p>
+
           </div>
+
         </div>
+
       </div>
       
     </div>
